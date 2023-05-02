649f38ff58c555b2b899ef1405fd04f16a9d4865
John Murianki <mcjthiongo@gmail.com>
2023-05-02 03:54:58 +0300

    Add functionality to checkboxes and delete icon

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
+import Sortable from 'sortablejs';
+import getSavedTodoItems from '../modules/createList.js';
+import checkboxCheck from '../modules/checkbox.js';
+import toggleDeleteIcon from '../modules/toggleicon.js';
 import './index.css';
 
 const todoListItem = document.querySelector('.activity');
 const todoList = document.getElementsByClassName('todo-list')[0];
 const clearButton = document.querySelector('.clear');
-let todoItems = [
-  {
-    description: 'create a new todo',
-    completed: false,
-    index: 0,
+let todoItems = [];
+
+Sortable.create(todoList, {
+  animation: 150,
+  ghostClass: 'blue-background-class',
+  onEnd: () => {
+    todoList.classList.add('sorted');
+
+    setTimeout(() => {
+      todoList.classList.remove('sorted');
+    }, 1000);
   },
-  {
-    description: 'create a new todo',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'create a new todo',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'create a new todo',
-    completed: false,
-    index: 3,
-  },
-];
+});
+
 function saveTodoItems() {
   localStorage.setItem('todoItems', JSON.stringify(todoItems));
 }
@@ -33,15 +29,6 @@ function fixindex() {
     todoItems[i].index = i;
   }
   saveTodoItems();
-}
-function checkboxCheck() {
-  const checkboxes = document.querySelectorAll('.checkbox');
-  for (let i = 0; i < checkboxes.length; i += 1) {
-    if (checkboxes[i].checked) {
-      todoItems[i].completed = true;
-    }
-  }
-  return todoItems;
 }
 
 function addTodo(todo) {
@@ -52,10 +39,6 @@ function addTodo(todo) {
   };
   todoItems.push(todolistObj);
   saveTodoItems();
-}
-function getSavedTodoItems() {
-  const savedData = localStorage.getItem('todoItems');
-  return savedData ? JSON.parse(savedData) : todoItems;
 }
 
 function editItem(label) {
@@ -80,12 +63,17 @@ function displayTodoItems() {
     checkbox.type = 'checkbox';
     checkbox.checked = todoItem.completed;
     checkbox.classList.add('checkbox');
+    checkbox.addEventListener('change', () => {
+      todoItem.completed = checkbox.checked;
+      saveTodoItems();
+    });
     const label = document.createElement('label');
     label.htmlFor = 'checkbox';
     label.innerText = todoItem.description;
     label.classList.add('edit-item');
     const icon = document.createElement('i');
     icon.className = 'fas fa-ellipsis-v';
+    icon.classList.add('icon');
     item.appendChild(checkbox);
     item.appendChild(label);
     item.appendChild(icon);
@@ -95,13 +83,26 @@ function displayTodoItems() {
     editItem(label);
   }
   fixindex();
+
+  const icons = Array.from(document.querySelectorAll('.icon'));
+
+  function toggleDeleteIcon(icon) {
+    icon.classList.toggle('fa-trash-alt');
+    icon.classList.toggle('fa-ellipsis-v');
+  }
+
+  icons.forEach((icon) => {
+    icon.addEventListener('mouseover', () => {
+      toggleDeleteIcon(icon);
+    });
+
+    icon.addEventListener('mouseout', () => {
+      toggleDeleteIcon(icon);
+    });
+  });
 }
 function deleteTodoItem() {
-  for (let i = todoItems.length - 1; i >= 0; i -= 1) {
-    if (todoItems[i].completed === true) {
-      todoItems.splice(i, 1);
-    }
-  }
+  todoItems = todoItems.filter((item) => item.completed === false);
   saveTodoItems();
   displayTodoItems();
 }
@@ -115,6 +116,21 @@ todoListItem.addEventListener('keydown', (event) => {
   }
 });
 clearButton.addEventListener('click', () => {
-  checkboxCheck();
+  checkboxCheck(todoItems);
   deleteTodoItem();
 });
+
+todoList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('fa-trash-alt')) {
+    const itemId = parseInt(event.target.closest('.list-item').getAttribute('data-id'), 10);
+    todoItems = todoItems.filter((item) => item.index !== itemId);
+    saveTodoItems();
+    displayTodoItems();
+  }
+});
+
+module.exports = {
+  editItem,
+  toggleDeleteIcon,
+  deleteTodoItem,
+};
